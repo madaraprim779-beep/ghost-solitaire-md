5e6e6e5c299264c5e7a77a182113fc9906d75c07
madaraprim779-beep <madaraprim779@gmail.com>
2026-09-09 20:15:47 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const config = {
+  botName: "GHOST SOLITAIRE MD",
+  ownerNumber: "2250709300922",
+  prefix: ".",
+
+  // Ne jamais mettre la vraie session directement dans GitHub.
+  sessionId: process.env.SESSION_ID || ""
+};
