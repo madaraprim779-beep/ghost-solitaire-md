c873924f7b564b69375d9dec5550d544e78e0376
madaraprim779-beep <madaraprim779@gmail.com>
2026-09-14 14:08:36 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export const config = {
-  botName: "GHOST SOLITAIRE MD",
-
-  // Numéro WhatsApp du compte qui sera connecté au bot
-  botNumber: process.env.BOT_NUMBER || "",
-
-  // Numéro du propriétaire
-  ownerNumber: "2250709300922",
-
-  prefix: "."
+    botName: process.env.BOT_NAME || "GHOST SOLITAIRE MD",
+    
+    // Numéro WhatsApp du compte qui sera connecté au bot (récupéré depuis Render)
+    botNumber: process.env.BOT_NUMBER || "",
+    
+    // Numéro du propriétaire (récupéré depuis Render, avec votre numéro actuel par défaut)
+    ownerNumber: process.env.OWNER_NUMBER || "2250709300922",
+    
+    prefix: process.env.PREFIX || "."
 };
