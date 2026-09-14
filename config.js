666a5dfb72cdb3c9e7cf6789b74e9265be39bc3b
madaraprim779-beep <madaraprim779@gmail.com>
2026-09-14 15:33:04 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 export const config = {
     botName: process.env.BOT_NAME || "GHOST SOLITAIRE MD",
     
-    // Numéro WhatsApp du compte connecté au bot
+    // Numéro WhatsApp du compte connecté au bot et du propriétaire
     botNumber: process.env.BOT_NUMBER || "2250565974887",
-    
-    // Numéro du propriétaire
     ownerNumber: process.env.OWNER_NUMBER || "2250565974887",
     
-    // Lien de l'image du bot (https://h.uguu.se/rLTCMeuL.jpg)
+    // Lien de l'image du bot
     botImage: process.env.BOT_IMAGE || "https://h.uguu.se/rLTCMeuL.jpg",
     
     prefix: process.env.PREFIX || "."
